12941e023739200c61881414cee4814a00abdcc0
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 15:56:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,5 +3,5 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift(bouroughs_in_nyc, @new_neighborhood)
-  bouroughs_in_nyc.unshift(@new_neighborhood)
+  bouroughs_in_nyc.unshift
 end
