94413fdc7df4c2c497698a4cc120ce65f88cec26
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 16:06:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_push(colors_in_the_rainbow, next_color)
   colors_in_the_rainbow.push(next_color)
 end
 
-def using_unshift(bouroughs_in_nyc, new_neighborhood)
+def using_unshift(bouroughs_in_nyc, @new_neighborhood)
   bouroughs_in_nyc = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
   @new_neighborhood = "Staten Island"
-  array.unshift
+  array.unshift("@new_neighborhood")
 end
