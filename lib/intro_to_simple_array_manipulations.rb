d5e858b95c044bc874154f90377b9aeaa9308668
Stacy Johns <stacykjohns@icloud.com>
2019-07-11 16:22:23 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ def using_unshift(bouroughs_in_nyc, new_neighborhood)
   bouroughs_in_nyc.unshift("Staten Island")
 end
 
-def using_pop = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+def using_pop
   using_pop.pop
 end
